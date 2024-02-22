b2925ea1ce37d641a27b4d1a19147b4e95c83b32
픽셀강박 <126048017+denshol@users.noreply.github.com>
2024-02-23 07:32:49 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 180ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240223/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.js
@@ -0,0 +1,11 @@
+function printStarsFive(){
+    let str ='';
+    for(let i=0;i<5;i++){
+        str='';
+        for(let j=0;j<10;j++){
+            str+='*';
+        }
+        console.log(str);
+    }
+}
+printStarsFive();
